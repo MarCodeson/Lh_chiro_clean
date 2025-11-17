15852fc10623aa5002a4a8bcda43a7f95c79dae1
MarCodeson <omardixon@hotmail.co.uk>
2025-11-17 17:11:47 +0000

    logo size increase edit

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -4,22 +4,30 @@ import Image from 'next/image'
 
 export function Hero() {
   return (
-    <section className='w-full overflow-hidden'>
-    <div
-      id="hero"
-      className="relative flex items-center justify-center w-screen h-screen overflow-hidden bg-white"
-    >
+    <section className="w-full overflow-hidden">
+      <div
+        id="hero"
+        className="relative flex items-center justify-center w-screen h-screen overflow-hidden bg-white"
+      >
+        <Image
+          src="/brand/lh-logo.png"
+          alt="LH Chiropractic logo"
+          width={1920}
+          height={1080}
+          priority
+          sizes="100vw"
+          className="
+            w-screen
+            h-auto
+            object-contain
 
-      <Image
-        src="/brand/lh-logo.png"
-        alt="LH Chiropractic logo"
-        width={1920}
-        height={1080}
-        sizes="100vw"
-        priority
-        className="w-screen h-auto object-contain"
-      />
-    </div>
+            /* MOBILE OVERRIDE */
+            max-w-none
+            scale-[1.9]
+            sm:scale-100
+          "
+        />
+      </div>
     </section>
   )
 }
